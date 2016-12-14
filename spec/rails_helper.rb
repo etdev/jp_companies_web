338f0eb774ecfbb139a89ecc7caeb4ad8559b296
Eric Turner <ericturnerdev@gmail.com>
2016-12-14 22:24:36 +0900

    Add basic gems

---
+++ spec/rails_helper.rb
@@ -0,0 +1,24 @@
+ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+
+ActiveRecord::Migration.maintain_test_schema!
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),"support","**","*.rb"))].each {|f| require f}
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
+end
