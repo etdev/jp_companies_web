3835ee8ce121a51cbbcc140a760cf19e787b92ae
Eric Turner <ericturnerdev@gmail.com>
2017-01-07 11:39:39 +0900

    Use i18n, prettify slim in dev and test envs

--- spec/features/user_views_home_page_spec.rb
+++ spec/features/user_views_home_page_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.feature "User views home page" do
   scenario "sees welcome message" do
     visit root_path
-    expect(page).to have_content("Welcome")
+    expect(page).to have_content(I18n.t("homes.show.welcome_message"))
   end
 end
--- spec/features/user_views_vorkers_entries_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.feature "User views vorkers entries" do
-  scenario "sees list of entries" do
-    #
-  end
-end
